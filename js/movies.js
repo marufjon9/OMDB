1d8f2c8ecbd4b14b59cec6ed69f3bfce2a11cb46
marufjon9 <mabduraupovv@gmail.com>
2023-09-01 19:55:23 +0500

    Check width

---
+++ js/movies.js
@@ -0,0 +1,44 @@
+movies = [
+  {
+    id: 1,
+    img: "https://m.media-amazon.com/images/M/MV5BODQ0OWJiMzktYjNlYi00MzcwLThlZWMtMzRkYTY4ZDgxNzgxXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX.png",
+    title: "Fight Club",
+    year: 1999,
+    type: ["Drama"],
+  },
+  {
+    id: 2,
+    img: "https://m.media-amazon.com/images/M/MV5BODYwNDYxNDk1Nl5BMl5BanBnXkFtZTgwOTAwMTk2MDE@._V1.png",
+    title: "Where the Crawdads Sing",
+    year: 2022,
+    type: ["Drama, Mystery, Romance"],
+  },
+  {
+    id: 3,
+    img: "https://m.media-amazon.com/images/M/MV5BZjQxOTY2MzYtZjhhNy00Y2ExLWJiMWYtYTNkMzA2ZTk5MDU4XkEyXkFqcGdeQXVyNTU5Mzk0NjE@._V1.png",
+    title: "Our Friend",
+    year: 2019,
+    type: ["Drama", "Romance"],
+  },
+  {
+    id: 4,
+    img: "https://m.media-amazon.com/images/M/MV5BMTMzODg4MjU4OF5BMl5BanBnXkFtZTYwNDAzNTU3._V1.png",
+    title: "The Painted Veil",
+    year: 2006,
+    type: ["Drama", "Romance"],
+  },
+  {
+    id: 5,
+    img: "https://m.media-amazon.com/images/M/MV5BNDg4NjM1YjMtYmNhZC00MjM0LWFiZmYtNGY1YjA3MzZmODc5XkEyXkFqcGdeQXVyNDk3NzU2MTQ@._V1_SX.png",
+    title: "Amélie",
+    year: 2006,
+    type: ["Comedy, Romance"],
+  },
+  {
+    id: 6,
+    img: "https://m.media-amazon.com/images/M/MV5BZTcxNzgzZjMtYzZiZC00MmE1LTg3MzQtZDAxMTYyZWE4MDNhL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1.png",
+    title: "You've Got Mail",
+    year: 1998,
+    type: ["Comedy", "Drama", "Romance"],
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,5 +1,38 @@
 const movieTheme = document.querySelector(".movie__theme");
+const mainList = document.querySelector(".main__list");
+const fYear = document.querySelector(".f__year");
 
 movieTheme.addEventListener("click", function () {
   document.body.classList.toggle("dark");
 });
+
+function renderMovies(array) {
+  array.forEach((element) => {
+    mainList.innerHTML += `
+      <li class="movie__item">
+        <img
+          dataset=${element.id}
+          src=${element.img}
+          alt=${element.title}
+          class="movie__img"
+          
+        />
+        <div class="movie__textbox">
+          <h2 class="movie__title">${element.title} <span>${element.year}</span></h2>
+          <p class="movie__type">${element.type}</p>
+        </div>
+        <button class="movie__fav" type="button"></button>
+      </li>
+    `;
+  });
+}
+
+renderMovies(movies);
+
+const date = new Date();
+
+const year = date.getFullYear();
+
+const thisYear = `${year}`;
+
+fYear.textContent = thisYear;
